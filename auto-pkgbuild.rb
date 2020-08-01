e0e90bdb98c76d8d7ca5c20ccee394a1d33a442b
Simone Scalabrino <s.scalabrino9@gmail.com>
2020-08-01 18:48:11 +0200

    Added dependency management

--- auto-pkgbuild.rb
+++ auto-pkgbuild.rb
@@ -6,7 +6,9 @@ git_tag = '0.1'
 Dir.glob("*").select { |e| FileTest.directory?(e) && !e.start_with?("_") }.each do |appname|
     info_file = JSON.parse(File.read("#{appname}/info.json"))
     
-    pkgbuild = template % [appname, info_file['desc'], git_tag]
+    dependencies = (['silos'] + info_file['dependencies'].to_a).map { |e| "'#{e}'" }.join(" ")
+    
+    pkgbuild = template % [appname, info_file['desc'], git_tag, dependencies]
     
     `mkdir -p "_dist/#{appname}"`
     File.open("_dist/#{appname}/PKGBUILD", 'w') do |f|
