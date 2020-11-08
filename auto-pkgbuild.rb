47b8a068d451610caf863aacf91c07728e34585d
Simone Scalabrino <s.scalabrino9@gmail.com>
2020-11-08 20:02:33 +0100

    Updated automatic pkgbuild

--- auto-pkgbuild.rb
+++ auto-pkgbuild.rb
@@ -1,14 +1,15 @@
 require 'json'
 
 template = File.read("PKGBUILD.template")
-git_tag = '0.1'
+GIT_TAG = '0.1'
 
 Dir.glob("*").select { |e| FileTest.directory?(e) && !e.start_with?("_") }.each do |appname|
     info_file = JSON.parse(File.read("#{appname}/info.json"))
     
     dependencies = (['silos'] + info_file['dependencies'].to_a).map { |e| "'#{e}'" }.join(" ")
     
-    pkgbuild = template % [appname, info_file['desc'], git_tag, dependencies]
+    version = info_file['version'] || GIT_TAG
+    pkgbuild = template % [appname, info_file['desc'], version, dependencies]
     
     `mkdir -p "_dist/#{appname}"`
     File.open("_dist/#{appname}/PKGBUILD", 'w') do |f|
